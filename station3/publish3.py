9b20141711020872e4fdc7756e3bebef3bdd6188
Yuri Pagani <yuripagani@gmail.com>
2021-09-08 10:01:04 -0300

    Added: IoT things and connection to AWS

---
+++ station3/publish3.py
@@ -0,0 +1,36 @@
+import os
+import time as t
+import json
+from dotenv import load_dotenv
+import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
+
+load_dotenv()
+
+# Define AWS parameters to connect via MQTT
+ENDPOINT = os.getenv("ENDPOINT")
+PATH_TO_CERT = os.getenv("CERTIFICATE_3")
+PATH_TO_KEY = os.getenv("PRIVATE_KEY_3")
+PATH_TO_ROOT = os.getenv("ROOT_3")
+
+# Parameters related to the specific station
+CLIENT_ID = "Air Quality Station 3"
+MESSAGE = "Hello from Station Three"
+TOPIC = "test/testing"
+RANGE = 20
+
+# Setting up connection with AWS IoT Core
+myAWSIoTMQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(CLIENT_ID)
+myAWSIoTMQTTClient.configureEndpoint(ENDPOINT, 8883)
+myAWSIoTMQTTClient.configureCredentials(PATH_TO_ROOT, PATH_TO_KEY, PATH_TO_CERT)
+
+# Starting send data via MQTT
+myAWSIoTMQTTClient.connect()
+print('Begin Publish')
+for i in range (RANGE):
+    data = "{} [{}]".format(MESSAGE, i+1)
+    message = {"message" : data}
+    myAWSIoTMQTTClient.publish(TOPIC, json.dumps(message), 1) 
+    print("Published: '" + json.dumps(message) + "' to the topic: " + "'test/testing'")
+    t.sleep(2)
+print('Publish End')
+myAWSIoTMQTTClient.disconnect()
