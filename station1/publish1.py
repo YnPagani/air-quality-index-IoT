3a0ad06e032ad48700955b9d4dd97429111460c4
Yuri Pagani <yuripagani@gmail.com>
2021-09-09 12:14:10 -0300

    Updated: Sending data from csv to AWS

--- station1/publish1.py
+++ station1/publish1.py
@@ -1,8 +1,11 @@
 import os
+import sys
 import time as t
 import json
 from dotenv import load_dotenv
 import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
+
+sys.path.append(".")
 
 load_dotenv()
 
@@ -14,23 +17,27 @@ PATH_TO_ROOT = os.getenv("ROOT_1")
 
 # Parameters related to the specific station
 CLIENT_ID = "Air Quality Station 1"
-MESSAGE = "Hello from Station One"
+LATITUDE, LONGITUDE = -23.588923, -46.658233
 TOPIC = "test/testing"
 RANGE = 20
 
-# Setting up connection with AWS IoT Core
-myAWSIoTMQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(CLIENT_ID)
-myAWSIoTMQTTClient.configureEndpoint(ENDPOINT, 8883)
-myAWSIoTMQTTClient.configureCredentials(PATH_TO_ROOT, PATH_TO_KEY, PATH_TO_CERT)
+if __name__ == "__main__":
+    from data_extraction import AirQualityData
+    # Setting up to get AQI data
+    aqi = AirQualityData("station1/ibirapuera_sp_aqi.csv")
+    
+    # Setting up connection with AWS IoT Core
+    myAWSIoTMQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(CLIENT_ID)
+    myAWSIoTMQTTClient.configureEndpoint(ENDPOINT, 8883)
+    myAWSIoTMQTTClient.configureCredentials(PATH_TO_ROOT, PATH_TO_KEY, PATH_TO_CERT)
 
-# Starting send data via MQTT
-myAWSIoTMQTTClient.connect()
-print('Begin Publish')
-for i in range (RANGE):
-    data = "{} [{}]".format(MESSAGE, i+1)
-    message = {"message" : data}
-    myAWSIoTMQTTClient.publish(TOPIC, json.dumps(message), 1) 
-    print("Published: '" + json.dumps(message) + "' to the topic: " + "'test/testing'")
-    t.sleep(2)
-print('Publish End')
-myAWSIoTMQTTClient.disconnect()
+    # Starting send data via MQTT
+    myAWSIoTMQTTClient.connect()
+    print('Begin Publish')
+    for i in range (RANGE):
+        message = {"id" : CLIENT_ID, "geolocation" : [LATITUDE, LONGITUDE], "payload" : aqi.get_data()}
+        myAWSIoTMQTTClient.publish(TOPIC, json.dumps(message), 1) 
+        print("Published: '" + json.dumps(message) + "\n")
+        t.sleep(2)
+    print('Publish End')
+    myAWSIoTMQTTClient.disconnect()
--- station2/publish2.py
+++ station2/publish2.py
@@ -1,8 +1,11 @@
 import os
+import sys
 import time as t
 import json
 from dotenv import load_dotenv
 import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
+
+sys.path.append(".")
 
 load_dotenv()
 
@@ -14,23 +17,28 @@ PATH_TO_ROOT = os.getenv("ROOT_2")
 
 # Parameters related to the specific station
 CLIENT_ID = "Air Quality Station 2"
-MESSAGE = "Hello from Station Two"
+LATITUDE, LONGITUDE = -23.546347, -46.627245
+
 TOPIC = "test/testing"
-RANGE = 20
+RANGE = 10
 
-# Setting up connection with AWS IoT Core
-myAWSIoTMQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(CLIENT_ID)
-myAWSIoTMQTTClient.configureEndpoint(ENDPOINT, 8883)
-myAWSIoTMQTTClient.configureCredentials(PATH_TO_ROOT, PATH_TO_KEY, PATH_TO_CERT)
+if __name__ == "__main__":
+    from data_extraction import AirQualityData
+    # Setting up to get AQI data
+    aqi = AirQualityData("station2/parque_dpedroii_sp_aqi.csv")
+    
+    # Setting up connection with AWS IoT Core
+    myAWSIoTMQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(CLIENT_ID)
+    myAWSIoTMQTTClient.configureEndpoint(ENDPOINT, 8883)
+    myAWSIoTMQTTClient.configureCredentials(PATH_TO_ROOT, PATH_TO_KEY, PATH_TO_CERT)
 
-# Starting send data via MQTT
-myAWSIoTMQTTClient.connect()
-print('Begin Publish')
-for i in range (RANGE):
-    data = "{} [{}]".format(MESSAGE, i+1)
-    message = {"message" : data}
-    myAWSIoTMQTTClient.publish(TOPIC, json.dumps(message), 1) 
-    print("Published: '" + json.dumps(message) + "' to the topic: " + "'test/testing'")
-    t.sleep(2)
-print('Publish End')
-myAWSIoTMQTTClient.disconnect()
+    # Starting send data via MQTT
+    myAWSIoTMQTTClient.connect()
+    print('Begin Publish')
+    for i in range (RANGE):
+        message = {"id" : CLIENT_ID, "geolocation" : [LATITUDE, LONGITUDE], "payload" : aqi.get_data()}
+        myAWSIoTMQTTClient.publish(TOPIC, json.dumps(message), 1) 
+        print("Published: '" + json.dumps(message) + "'\n")
+        t.sleep(2)
+    print('Publish End')
+    myAWSIoTMQTTClient.disconnect()
