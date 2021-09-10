886f4cea976b2c92565504fa701c6330b16c12bc
Yuri Pagani <yuripagani@gmail.com>
2021-09-10 17:50:06 -0300

    Updated: Formatted the payload to be sent to AWS

--- data_extraction.py
+++ data_extraction.py
@@ -3,10 +3,28 @@ import pandas as pd
 
 class AirQualityData:
     def __init__(self, path: str) -> None:
-        self.__data_frame = pd.read_csv(Path(path))
-        self.__iterator = self.__data_frame.iterrows()
+        self.data_frame = pd.read_csv(Path(path))
+        self.__iterator = self.data_frame.iterrows()
 
         
     def get_data(self):
-        return next(self.__iterator)[1].to_dict()        
+        # Dictionary used to store process data that will be sent
+        process_data = dict()
         
+        # Get csv next line raw data
+        raw_data = next(self.__iterator)[1].to_dict()
+        
+        # Iterates over the raw data eliminating all unnecessary white space. 
+        # Change the AQI value type from Str to Int. (For empty strings, the defined value is -1)
+        for key, item in raw_data.items():
+            if key == "date":
+                process_data[key.lstrip()] = item    
+            else:
+                try:
+                    process_item = int(item.lstrip())
+                except ValueError:
+                    if not item.lstrip():
+                        process_item = -1
+                
+                process_data[key.lstrip()] = process_item
+        return process_data
